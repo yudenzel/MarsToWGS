1e0cf21a95fc500bc0ee4646eef0cdc8b8ea0907
NetCharm <NetCharm@localhost>
2013-07-11 01:15:17 +0800

    1. update .hgignore contents 2. change app version-number

--- Mars2WGS/Properties/AssemblyInfo.cs
+++ Mars2WGS/Properties/AssemblyInfo.cs
@@ -25,12 +25,12 @@ using System.Runtime.InteropServices;
 // 程序集的版本信息由下面四个值组成:
 //
 //      主版本
-//      次版本 
+//      次版本
 //      内部版本号
 //      修订号
 //
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.2.0.79" )]
+[assembly: AssemblyVersion( "1.2.*" )]
 [assembly: AssemblyFileVersion( "1.2.0.0" )]
